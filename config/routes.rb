7a2cd0dc96a66dfcc53075e568e433af208ce02f
Jovem-Blood <thiagopeixot13@gmail.com>
2022-04-20 20:19:59 -0300

    design some .erb files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'articles#index' 
-  devise_for :users
+  root 'articles#index'
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
 
   resources :articles do
     resources :comments
