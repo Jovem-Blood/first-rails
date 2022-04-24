b86208c6c232b9f0854c5a034a36ab3ac2ad9a21
Jovem-Blood <thiagopeixot13@gmail.com>
2022-04-23 23:09:10 -0300

    add a path to redirect when sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'articles#index'
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
+  devise_for :users, path: '',
+                     path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
   get 'my-articles', to: 'articles#user_articles'
   resources :articles do
